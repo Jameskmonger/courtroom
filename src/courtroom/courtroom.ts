01a0f9f5987701dc6da12e4db138aba39cdaceb8
James Monger <jameskmonger@hotmail.co.uk>
2015-12-10 18:25:08 +0000

    Allow trialling of properties

--- src/courtroom/courtroom.ts
+++ src/courtroom/courtroom.ts
@@ -1,7 +1,13 @@
 export class Courtroom {
   public defendantCount: number;
+  private defendants: Array<any>;
 
   constructor() {
     this.defendantCount = 0;
+    this.defendants = [];
+  }
+
+  public trial(name: string) {
+    this.defendants.push(name);
   }
 }
