1024b96075226a8c5726e2e7b5df8e53b0ec15af
James Monger <jameskmonger@hotmail.co.uk>
2015-12-14 12:53:26 +0000

    Remove untested code from courtroom

--- src/courtroom/courtroom.ts
+++ src/courtroom/courtroom.ts
@@ -1,13 +1,6 @@
 export class Courtroom {
-  public defendantCount: number;
-  private defendants: Array<any>;
 
   constructor() {
-    this.defendantCount = 0;
-    this.defendants = [];
-  }
 
-  public trial(name: string) {
-    this.defendants.push(name);
   }
 }
