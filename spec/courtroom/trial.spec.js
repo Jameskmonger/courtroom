07940c40dbcf1f6b294827ef09e9f743c9c1e524
James Monger <jameskmonger@hotmail.co.uk>
2015-12-14 12:42:15 +0000

    Add constructor value tests

--- spec/courtroom/trial.spec.js
+++ spec/courtroom/trial.spec.js
@@ -2,6 +2,22 @@ define(['../../build/trial'], function(TrialModule) {
   var Trial = TrialModule.Trial;
 
   describe("Constructor", function() {
+    it("stores the name correctly for 'property'", function() {
+      var expectedName = "property";
+
+      var t = new Trial(expectedName);
+
+      expect(t.getName()).toBe(expectedName);
+    });
+
+    it("stores the name correctly for 'value'", function() {
+      var expectedName = "value";
+
+      var t = new Trial(expectedName);
+
+      expect(t.getName()).toBe(expectedName);
+    });
+
     it("has no accusations", function() {
       var t = new Trial("prop");
 
--- src/courtroom/trial.ts
+++ src/courtroom/trial.ts
@@ -1,3 +1,9 @@
 export class Trial {
+  constructor(private name: string) {
 
+  }
+
+  public getName(): string {
+    return this.name;
+  }
 }
