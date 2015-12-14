19e458d38a6d5989c6cc1ca5f080040ef6bb9fba
James Monger <jameskmonger@hotmail.co.uk>
2015-12-14 22:44:09 +0000

    Added tests for more types in NotJury

--- spec/courtroom/juries/notjury.spec.js
+++ spec/courtroom/juries/notjury.spec.js
@@ -26,4 +26,60 @@ define(['../../../build/juries/notjury'], function(NotJuryModule) {
       expect(a.verdict(undefined)).toBe(false);
     });
   });
+
+  describe("NotJury verdict tests (numeric)", function() {
+    it("should return 'true' when integer numbers don't match", function() {
+      var a = new NotJury(5);
+
+      expect(a.verdict(3)).toBe(true);
+    });
+
+    it("should return 'false' when integer numbers match", function() {
+      var a = new NotJury(5);
+
+      expect(a.verdict(5)).toBe(false);
+    });
+
+    it("should return 'true' when decimal numbers don't match", function() {
+      var a = new NotJury(5.25);
+
+      expect(a.verdict(3.6)).toBe(true);
+    });
+
+    it("should return 'false' when decimal numbers match", function() {
+      var a = new NotJury(5.25);
+
+      expect(a.verdict(5.25)).toBe(false);
+    });
+  });
+
+  describe("NotJury verdict tests (string)", function() {
+    it("should return 'true' when strings don't match", function() {
+      var a = new NotJury("james");
+
+      expect(a.verdict("william")).toBe(true);
+    });
+
+    it("should return 'false' when strings match", function() {
+      var a = new NotJury("james");
+
+      expect(a.verdict("james")).toBe(false);
+    });
+  });
+
+  describe("NotJury verdict tests (object)", function() {
+    it("should return 'true' when objects don't match", function() {
+      var a = new NotJury(new NotJury(null));
+
+      expect(a.verdict(new NotJury(null))).toBe(true);
+    });
+
+    it("should return 'false' when objects match", function() {
+      var dummy = new NotJury(null);
+
+      var a = new NotJury(dummy);
+
+      expect(a.verdict(dummy)).toBe(false);
+    });
+  });
 });
