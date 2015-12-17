a1b67aabf59adf83cd84487ea9d848722ede3d9d
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 00:13:51 +0000

    Added string.minLength spec

---
+++ spec/courtroom/juries/string.minLength.spec.ts
@@ -0,0 +1,8 @@
+///<reference path="../../../typings/jasmine/jasmine.d.ts" />
+
+import CourtroomModule = require("../../../src/courtroom/courtroom");
+const Courtroom = CourtroomModule.Courtroom;
+
+describe("minLength tests", function() {
+
+});
