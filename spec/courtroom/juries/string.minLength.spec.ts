6e8dc179b7527826dc8cebd4c4c3fe3001ac6962
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 00:15:23 +0000

    Add first minlength tests

--- spec/courtroom/juries/string.minLength.spec.ts
+++ spec/courtroom/juries/string.minLength.spec.ts
@@ -4,5 +4,31 @@ import CourtroomModule = require("../../../src/courtroom/courtroom");
 const Courtroom = CourtroomModule.Courtroom;
 
 describe("minLength tests", function() {
+  it("should return no issues if minimum length is 0 and property is empty string", function() {
+    let c = new Courtroom();
 
+    c.trial("prop").laws.minLength(0);
+
+    let dummy = {
+      prop: ""
+    };
+
+    let issues = c.judge(dummy);
+
+    expect(issues).toEqual([]);
+  });
+
+  it("should return no issues if minimum length is 0 and property is 'james'", function() {
+    let c = new Courtroom();
+
+    c.trial("prop").laws.minLength(0);
+
+    let dummy = {
+      prop: "james"
+    };
+
+    let issues = c.judge(dummy);
+
+    expect(issues).toEqual([]);
+  });
 });
