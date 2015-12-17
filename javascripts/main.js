c6d604016e35b5d06e969a41aeb84caad250f8e3
James Monger <jameskmonger@hotmail.co.uk>
2015-12-17 12:53:25 +0000

    Set up the laws

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,5 +4,10 @@ require(["courtroom/courtroom"], function(CourtroomModule) {
   const Courtroom = CourtroomModule.Courtroom;
 
   var c = new Courtroom();
-  
+  c.trial("min_5").laws.minLength(5);
+  c.trial("max_5").laws.maxLength(5);
+  c.trial("contains_hello").laws.contains("hello");
+  c.trial("equals_james").laws.is("james");
+  c.trial("not_equals_paulo").laws.not("paulo");
+
 });
