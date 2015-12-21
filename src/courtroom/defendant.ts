3ed2c2230a6a9a782f7e9ef439a37e5ead916950
James Monger <jameskmonger@hotmail.co.uk>
2015-12-21 18:15:30 +0000

    Added passing tests around defendant child count

--- src/courtroom/defendant.ts
+++ src/courtroom/defendant.ts
@@ -2,13 +2,23 @@ import Lawbook = require("./lawbook");
 
 export class Defendant {
   public laws: Lawbook.Lawbook;
+  private children: Array<string>;
 
   constructor(private name: string) {
     this.laws = new Lawbook.Lawbook();
+    this.children = [];
   }
 
   public getName(): string {
     return this.name;
+  }
+
+  public trial(name: string) {
+    this.children.push(name);
+  }
+
+  public getChildCount() {
+    return this.children.length;
   }
 
   public judge(value: any): Array<any> {
