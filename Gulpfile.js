d2d099fe2b765636c4ad2da6d46f3d14ba6428dc
Simon Adcock <simonadcock@notonthehighstreet.com>
2016-01-07 19:08:15 +0000

    Add CommonJS build task

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,12 +2,20 @@ const gulp = require('gulp');
 const tsc = require('gulp-typescript');
 const Server = require('karma').Server;
 const tsProject = tsc.createProject('src/tsconfig.json');
+const tsProjectCommonJS = tsc.createProject('src/tsconfig.json', {
+  module: 'commonjs'
+});
 const tsProjectSpec = tsc.createProject('spec/tsconfig.json');
 
 gulp.task('default', ['build']);
 gulp.task('build', () => {
   return tsProject.src()
     .pipe(tsc(tsProject))
+    .pipe(gulp.dest('build'));
+});
+gulp.task('build:commonjs', () => {
+  return tsProjectCommonJS.src()
+    .pipe(tsc(tsProjectCommonJS))
     .pipe(gulp.dest('build'));
 });
 gulp.task('test', ['test-build:spec', 'test-build:src'], () => {
