16f5f286092aba88e92beb59bd57df6699a83324
James Monger <jameskmonger@hotmail.co.uk>
2016-01-15 11:57:19 +0000

    Add UMD build and test to Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -18,7 +18,31 @@ gulp.task('build:commonjs', () => {
     .pipe(tsc(tsProjectCommonJS))
     .pipe(gulp.dest('.'));
 });
+gulp.task('build:umd', () => {
+  const tsProjectUMD = tsc.createProject('src/tsconfig.json', {
+    module: 'umd'
+  });
+
+  return tsProjectUMD.src()
+    .pipe(tsc(tsProjectUMD))
+    .pipe(gulp.dest('.'));
+});
 gulp.task('test', ['test-build:spec', 'test-build:src'], () => {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true,
+    plugins: [
+      'karma-jasmine',
+      'karma-requirejs',
+      'karma-spec-reporter',
+      'karma-coverage',
+      'karma-coveralls',
+      'karma-phantomjs-launcher',
+      'karma-chrome-launcher'
+    ]
+  }).start();
+});
+gulp.task('test:umd', ['test-build:spec', 'test-build:src:umd'], () => {
   new Server({
     configFile: __dirname + '/karma.conf.js',
     singleRun: true,
@@ -44,6 +68,17 @@ gulp.task('test-build:spec', (done) => {
 gulp.task('test-build:src', (done) => {
   tsProject.src()
     .pipe(tsc(tsProject))
+    .pipe(gulp.dest('test-build/src'))
+    .on('end', done);
+});
+
+gulp.task('test-build:src:umd', (done) => {
+  const tsProjectUMD = tsc.createProject('src/tsconfig.json', {
+    module: 'umd'
+  });
+	
+  tsProjectUMD.src()
+    .pipe(tsc(tsProjectUMD))
     .pipe(gulp.dest('test-build/src'))
     .on('end', done);
 });
