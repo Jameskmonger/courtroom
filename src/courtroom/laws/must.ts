fe7a01dbb6f688a01de0c16b35cab3cb0cae6200
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 13:48:53 +0000

    Store the must function

--- src/courtroom/laws/must.ts
+++ src/courtroom/laws/must.ts
@@ -2,8 +2,10 @@ import { Law } from './law';
 
 export class MustLaw implements Law {
 
-    constructor (mustFunction: (...args: any[]) => boolean) {
+    private _mustFunction: (...args: any[]) => boolean;
 
+    constructor (mustFunction: (...args: any[]) => boolean) {
+        this._mustFunction = mustFunction;
     }
 
     public verdict(defendant: any): boolean {
