a8049292c3db9a2f94815c3a0f2f18516684e2da
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 13:44:47 +0000

    Test case 1 for number value

--- spec/laws/must.spec.ts
+++ spec/laws/must.spec.ts
@@ -69,6 +69,24 @@ describe('MustLaw', () => {
             expect(functionCalled).to.be(true);
         });
 
+        it('should call into function with given numer [test case 1]', () => {
+            let givenNumber = 4;
+
+            let functionCalled = false;
+            let mustFunction = function func (str: number) {
+                if (str === givenNumber) {
+                    functionCalled = true;
+                }
+
+                return false;
+            };
+
+            let law = new MustLaw(mustFunction);
+            law.verdict(givenNumber);
+
+            expect(functionCalled).to.be(true);
+        });
+
     });
 
 });
