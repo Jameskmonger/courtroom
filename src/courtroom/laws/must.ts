1f9226f02c4a7dc014e674630b799006a528766b
James Monger <jameskmonger@hotmail.co.uk>
2016-03-26 13:28:57 +0000

    Add must.ts

---
+++ src/courtroom/laws/must.ts
@@ -0,0 +1,16 @@
+import { Law } from './law';
+
+export class MustLaw implements Law {
+
+    public verdict(defendant: any): boolean {
+        return false;
+    }
+
+    public getName(): string {
+        return 'must';
+    }
+
+    public getDetails(): any {
+        return {};
+    }
+}
